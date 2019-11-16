520a5b6b489c4979fc0e1fecdbe566caeef5f3e5
SinthujanBalachandran <sinthujan2233@gmail.com>
2019-11-16 06:35:06 -0800

    base structure

---
+++ sgic_defect_tracker/product-service/src/test/java/com/sgic/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgic.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
